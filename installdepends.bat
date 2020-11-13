f4788db3f2aee702e47f6c63db11d8e3ae4c2797
Reyansh-Khobragade <74308448+Reyansh-Khobragade@users.noreply.github.com>
2020-11-13 14:04:18 +0530

    Update installdepends.bat

--- installdepends.bat
+++ installdepends.bat
@@ -1,4 +1,4 @@
 @echo "Installing Dependencies =)"
-npm install discord.js, ytdl-core, node-opus, ffmpeg, jimp
+npm install discord.js ytdl-core node-opus ffmpeg jimp
 @echo "Installed All Dependencies :D"
 PAUSE
